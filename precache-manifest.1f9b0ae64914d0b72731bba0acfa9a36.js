b6914406dac370e8f245ae29654b44da98c8fe76
韩毅 <2387870957@qq.com>
2023-01-07 20:39:10 +0800

    deploy

---
+++ precache-manifest.1f9b0ae64914d0b72731bba0acfa9a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c1658b84468f29cc81",
+    "url": "/money-pool-lite-website/css/app.77170bc6.css"
+  },
+  {
+    "revision": "053ae4af0ded677282f6c9ca7373eb65",
+    "url": "/money-pool-lite-website/index.html"
+  },
+  {
+    "revision": "d3c1658b84468f29cc81",
+    "url": "/money-pool-lite-website/js/app.6027660f.js"
+  },
+  {
+    "revision": "a3b28541d27585dc5856",
+    "url": "/money-pool-lite-website/js/chunk-vendors.b4ca0c98.js"
+  },
+  {
+    "revision": "b3c5cb98105f965e1efc8882a42ad8d1",
+    "url": "/money-pool-lite-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-pool-lite-website/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/money-pool-lite-website/precache-manifest.1f9b0ae64914d0b72731bba0acfa9a36.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "money-pool-lite"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
